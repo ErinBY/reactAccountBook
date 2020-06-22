307c15be564900f0759df71d46452924e8b9fb86
seyoungb <se.young492@gmail.com>
2020-06-22 22:59:49 +0900

    Do-it react chap1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="title">Do it! First Class -!</h1>
         <p>
-          Hi! <code>src/App.js</code> and save to reload.
+          Hello! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
